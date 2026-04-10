64316894fc47eb1d041bbec9a1f8fa270dd71429
byteD-x <2041487752dxj@gmail.com>
2026-04-10 12:06:33 +0800

    docs: 发布 GitHub Pages 静态站点

---
+++ _next/static/Lc6lNaLVIZb8UhtoULlCC/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/My-Resume//_next/:path+",
+        "destination": "/My-Resume/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
